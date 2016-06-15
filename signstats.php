a7c120b018ac203dc8250f91233b0efb494ec388
The Dark Power Invader <admin@0group.gq>
2016-06-15 21:59:17 +0530

    Update signstats.php

--- signstats.php
+++ signstats.php
@@ -6,12 +6,16 @@
      * See http://php.net/manual/en/function.imagettftext.php to know about text on images using TTF/OTF Fonts.
      */
 
-    if (!isset($_SERVER['HTTP_REFERER']))die('Direct access to file is not allowed');
-    define('IN_MYBB',1);
-    define('NO_ONLINE',1);
-    require_once './global.php';
-    $lang->load('stats');
-    $stats=$cache->read('stats');
+    if (!isset($_SERVER['HTTP_REFERER']))die('Direct access to file is not allowed'); //disallow users to not open the file by typing URL
+    //-----------Start MyBB integration----------------
+    define('IN_MYBB',1); //required don't change
+    define('NO_ONLINE',1); //required don't change
+    require_once './global.php'; //main file to read statstics
+    $lang->load('stats'); //function to load stats variables
+    $stats=$cache->read('stats'); //cache the stats values
+    //------------End MyBB integration-----------------
+    
+    //----------Start Header section-------------------
     header('Pragma: public');
     header('Cache-Control: max-age=240');
     header('Expires: '. gmdate('D, d M Y H:i:s \G\M\T', time() + 240));
@@ -20,10 +24,14 @@
         exit; 
         } 
     header("Content-Type: image/png");
-    $bg = 'stats.png';
-    $font = 'roung.otf';
-    $forum = 'Post4VPS.com Free VPS Forum';
-
+    //----------End Header section--------------------
+    
+    $bg = 'stats.png'; //background file
+    $font = 'roung.otf'; //font file
+    $forum = 'Post4VPS.com Free VPS Forum'; //forum name or description
+    
+    //------------------------------------------------------------------
+    //PHP function to write text on image using font
     $img = imagecreatefrompng($bg);
     $fontcol = imagecolorallocate($img, 255, 255, 0);
     imagettftext($img,11,0,20,30,$fontcol,$font,utf8_decode($lang->members).' '.$stats['numusers']);
